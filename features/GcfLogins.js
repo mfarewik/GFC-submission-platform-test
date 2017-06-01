d989e629317e8918b74170f3adba8fb4da8bba7c
markus farewik <markus@fare-c.com>
2017-06-01 09:52:35 +0200

    New tests and debug information has been added

--- features/GcfLogins.js
+++ features/GcfLogins.js
@@ -1,37 +1,51 @@
 import { Selector } from 'testcafe';
+import { ClientFunction } from 'testcafe';
+
+const email = "m8r-qkyx87@mailinator.com";
+const password = "Testtest1";
+const LOGIN_CONTAINER = Selector('.app-col-right');
+const LOGIN_BUTTON = Selector('.gcf-btn-blue').nth(1);
+const LOGOUT_BUTTON = Selector('a.i.ion-android-exit');
 
 
 fixture `init test Global Challenge Foundation Submission`
     .page (`https://gcf-beta.koslun.com/en/submission-platform/sign-in`)
     .beforeEach(async t => {
         await t
-        .expect(LOGIN_BUTTON.visible).ok()
-        .expect(LOGIN_CONTAINER.find(' input[type="email"]').exists).ok()
-        .typeText(LOGIN_CONTAINER.find('input[type="email"]'), email)
-        .expect(LOGIN_CONTAINER.find(' input[type="password"]').exists).ok()
-        .typeText(LOGIN_CONTAINER.find(' input[type="password"]'), password)
+    .expect(LOGIN_BUTTON.visible).ok()
+    .expect(LOGIN_CONTAINER.find(' input[type="email"]').exists).ok()
+    .typeText(LOGIN_CONTAINER.find('input[type="email"]'), email)
+    .expect(LOGIN_CONTAINER.find(' input[type="password"]').exists).ok()
+    .typeText(LOGIN_CONTAINER.find(' input[type="password"]'), password)
+    .wait(1000)
+
     })
 
-const email = "m8r-qkyx87@mailinator.com";
-const password = "Testtest1";
-const LOGIN_CONTAINER = Selector('.app-col-right');
-const LOGIN_BUTTON = Selector('.gcf-btn-blue').nth(1);
-const LOGOUT_BUTTON = Selector('.ion-android-exit');
-
-
-test('GCF LOGIN test', async t => {
-  // LOGIN  navigation
+  // Cookie navigation
+test('GCF Cookie Notefication Test', async t => {
+  //debugger;
   await t
-    .expect(Selector('body').exists).ok()
 
-    .wait(3000)
-    .click(LOGIN_BUTTON)
+    .expect(Selector('#accept-button').exists).ok()
+    .click(Selector('#read-more'))
+    .expect(Selector('div.popup-title').exists).ok()
+    .click('.gcf-btn-blue')
+    .click(Selector('#accept-button'))
   });
 
-test('GCF LOGOUT test', async t => {
-    //LOGOUT navigation
-    await t
-    .click(LOGIN_BUTTON)
-    .click(LOGOUT_BUTTON)
+  // LOGIN  navigation
+test('GCF LOGIN/LOGOUT Test', async t => {
 
-});
+  await t
+
+    .expect(Selector('#accept-button').exists).ok()
+    .click(Selector('#accept-button'))
+  //  .click(Selector('.gcf-btn-blue'))
+
+    .expect(Selector('body').exists).ok()
+    .wait(500)
+    .click(LOGIN_BUTTON)
+    .wait(500)
+    .expect(Selector('div.gcf-title').exists).ok()
+    .click(Selector('.ion-android-exit'))
+  });
