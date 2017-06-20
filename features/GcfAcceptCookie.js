f1ab9c25577e3c9a81c9ee35c47def615781d692
markus farewik <markus@fare-c.com>
2017-06-20 17:42:19 +0200

    Work in progress page-model not yet implemented.

--- features/GcfAcceptCookie.js
+++ features/GcfAcceptCookie.js
@@ -3,6 +3,7 @@ import config from './config';
 import Page from '../genericFuncitons/page-model';
 
 const REGISTER_BUTTON = Selector('.gcf-btn-blue').nth(0);
+const page = new Page();
 
 fixture `init test Global Challenge Foundation Submission`
   .page `${config.baseUrl}/en/submission-platform/sign-in`
@@ -17,21 +18,21 @@ fixture `init test Global Challenge Foundation Submission`
 test('GCF Cookie Notefication Test', async t => {
     await t.wait(5000)
 
-    //.expect(Selector('#accept-button').exists).ok()
-    //.click(Selector('#read-more'))
-    //.expect(Selector('div.popup-title').exists).ok()
-    //.click('.gcf-btn-blue')
-    //.expect(Selector('#accept-button').exists).ok()
-    //.click(Selector('#accept-button'))
+    .expect(Selector('#accept-button').exists).ok()
+    .click(Selector('#read-more'))
+    .expect(Selector('div.popup-title').exists).ok()
+    .click('.gcf-btn-blue')
+    .expect(Selector('#accept-button').exists).ok()
+    .click(Selector('#accept-button'))
 
-for (const acceptCookieFeature of page.acceptCookieFeatureList){
-  await t.wait(10000)
-    .expect(acceptCookieFeature.acceptCookieId.exists).ok()
-    .click(acceptCookieFeature.acceptCookieId)
-    .expect(acceptCookieFeature.acceptCookieDiv.exists).ok()
-    .click(acceptCookieFeature.acceptCookieClass)
-    .expect(acceptCookieFeature.acceptCookieId.exists).ok()
-    .click(acceptCookieFeature.acceptCookieId)
-
-    }
+// for (const acceptCookieFeature of page.acceptCookieFeatureList){
+//   await t
+//     .expect(acceptCookieFeature.acceptCookieId.exists).ok()
+//     .click(acceptCookieFeature.readmoreCookieId)
+//     .expect(acceptCookieFeature.acceptCookiePopup.exists).ok()
+//     .click(acceptCookieFeature.acceptCookieClass)
+//     .expect(acceptCookieFeature.acceptCookieId.exists).ok()
+//     .click(acceptCookieFeature.acceptCookieId)
+//
+//     }
   });
