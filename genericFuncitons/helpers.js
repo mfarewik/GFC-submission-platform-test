48e85871223f51ad58295d9c61cef080269b8ba5
markus farewik <markus@fare-c.com>
2017-06-29 16:57:27 +0200

    Added function writeToFile(file, obj)

--- genericFuncitons/helpers.js
+++ genericFuncitons/helpers.js
@@ -2,15 +2,24 @@
 
 import { Selector, getTime , ClientFunction} from 'testcafe';
 
+var jsonfile = require('jsonfile')
+var file = '../../../data/data.json'
+
 export function randMs(){
   const d = new Date();
   var randomMsec = d.getTime();
   return randomMsec;
   }
 
-export function eMailAdress(){
+export function eMailAdress(prefix){
   const d = new Date();
   var randomMsec = d.getTime();
-  var emailadress = 'GCF' + randomMsec + '@mailinator.com';
+  var emailadress = prefix + randomMsec + '@mailinator.com';
   return emailadress;
+}
+
+export function writeToFile(file, obj){
+  jsonfile.writeFile(file, obj, {flag: 'a'}, function (err) {
+    console.error(err)
+  })
 }
