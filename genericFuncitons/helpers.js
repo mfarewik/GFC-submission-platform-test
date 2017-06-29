3c58fc0d2ad30a9977915da9f7351d6d5f167fe0
markus farewik <markus@fare-c.com>
2017-06-29 10:20:01 +0200

    added a function eMailAdress.

--- genericFuncitons/helpers.js
+++ genericFuncitons/helpers.js
@@ -1,6 +1,16 @@
 // Helpers class: write to file
-export async function randMs(){
+
+import { Selector, getTime , ClientFunction} from 'testcafe';
+
+export function randMs(){
   const d = new Date();
   var randomMsec = d.getTime();
   return randomMsec;
   }
+
+export function eMailAdress(){
+  const d = new Date();
+  var randomMsec = d.getTime();
+  var emailadress = 'GCF' + randomMsec + '@mailinator.com';
+  return emailadress;
+}
